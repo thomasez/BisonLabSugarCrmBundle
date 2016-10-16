52b3ca36e91d4893670b83f997c85284e47d6200
Thomas Lundquist <thomasez@bisonlab.no>
2016-10-16 16:49:51 +0200

    Adding the wrapper as an available service.

--- DependencyInjection/BisonLabSugarCrmBundleExtension.php
+++ DependencyInjection/BisonLabSugarCrmBundleExtension.php
@@ -24,5 +24,6 @@ class BisonLabSugarCrmExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+        $loader->load('services_wrapper.yml');
     }
 }
