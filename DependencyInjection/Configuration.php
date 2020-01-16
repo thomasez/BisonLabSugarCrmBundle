1e8b5dce3cdc762e9b8b6970d39db4625e96df41
Dabaru Application User <dabaru@dabaru-dev.nteb.no>
2020-01-16 20:34:53 +0100

    Too early.

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,6 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder('sugarcrm');
+        $rootNode = $treeBuilder->root('sugarcrm');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
