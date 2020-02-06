7090035eaea79c1acf50920f4f4cac7d6d36ccff
Dabaru Application User <dabaru@dabaru-dev.nteb.no>
2020-02-06 11:19:18 +0100

    Adding platform as an option since Sugar have started complaining about not knowing the platform (and api as a default seems too generic and is not a part of Sugars standard platforms anyway.)

--- Service/SugarWrapper.php
+++ Service/SugarWrapper.php
@@ -9,15 +9,20 @@ namespace BisonLab\SugarCrmBundle\Service;
 
 class SugarWrapper 
 {
-
     private $sugar;
     private $options;
 
-    public function __construct($base_url, $username, $password)
+    public function __construct($base_url, $username, $password, $platform = "sugar-wrapper")
     {
-        $this->options = array('base_url' => $base_url, 
-            'username' => $username, 
-            'password' => $password);
+        if (!preg_match("/rest\/v\d\d/", $base_url)) {
+            $base_url .= '/rest/v11_5/';
+        }
+        $this->options = [
+            'base_url' => $base_url,
+            'username' => $username,
+            'password' => $password,
+            'platform' => $platform
+        ];
     }
 
 
@@ -30,15 +35,12 @@ class SugarWrapper
 
     public function connectSugar()
     {
-        /* A base URL can include the path.. aka check if someone has..*/
-        if (!preg_match("/v10/", $this->options['base_url'])) {
-            $this->options['base_url'] = $this->options['base_url'] . '/rest/v10/';
-        }
         $this->sugar = new \Spinegar\SugarRestClient\Rest();
         $this->sugar
             ->setUrl($this->options['base_url'])
             ->setUsername($this->options['username'])
             ->setPassword($this->options['password'])
+            ->setPlatform($this->options['platform'])
             ;
     }
 }
